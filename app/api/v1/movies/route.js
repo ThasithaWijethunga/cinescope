e242a9a19cd112fe6aa8e0737a61670d98f46567
Thasitha Wijethunga <127384669+ThasithaWijethunga@users.noreply.github.com>
2025-07-08 23:19:23 +0530

    replace hardcoded data with MongoDB fetch

---
+++ app/api/v1/movies/route.js
@@ -0,0 +1,24 @@
+// import { MOVIES } from "@/lib/data";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    const movies = await db
+      .collection("movies")
+      .find({})
+      .sort({ metacritic: 1 })
+      .limit(20)
+      .toArray();
+
+    // return NextResponse.json(MOVIES, { status: 200 });
+    return NextResponse.json(movies);
+  } catch (error) {
+    console.log("Error fetching movies:", error);
+
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
--- components/home/movies-list.jsx
+++ components/home/movies-list.jsx
@@ -1,48 +1,43 @@
-import { Search } from "lucide-react";
-import { Input } from "@/components/ui/input";
 import { MOVIES } from "@/lib/data";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { getMovies } from "@/actions/movies";
 
-export default function MoviesList() {
+export default async function MoviesList() {
+  const movies = await getMovies();
+
+  if (!movies) {
+    return <div> No movies found</div>;
+  }
+
+  console.log("Movies : ", movies);
+
+  {
+    /* Movie card here */
+  }
   return (
-    <div className="space-y-6">
-      <div className="border-primary/20 bg-card shadow-xs rounded-lg border p-4">
-        <div className="flex flex-col gap-4">
-          <div className="relative">
-            <Search className="text-primary/70 absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2" />
-
-            {/*Shadcn UI Input component*/}
-            <Input
-              placeholder="Search movies by title or director"
-              className="border-primary/20 pl-9"
-            />
-          </div>
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {/* Loop movies */}
+      {/* {MOVIES.map((movie) => ( */} {/* hardcode */}
+      {movies.map((movie, index) => (
+        <div key={`${movie.id}-${index}`}>
+          <Link href={`/movies/${movie.id}`}>
+            <Card className="border-primary/20 hover:border-primary/50 overflow-hidden py-0 transition-colors">
+              <div className="aspect-2/3 w-full overflow-hidden">
+                <Image
+                  width={300}
+                  height={450}
+                  src={movie.poster || "./placeholder.svg"}
+                  alt={movie.title}
+                  className="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
+                  priority
+                />
+              </div>
+            </Card>
+          </Link>
         </div>
-      </div>
-
-      {/* Movie card here */}
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {/* Loop movies */}
-        {MOVIES.map((movie) => (
-          <div key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>
-              <Card className="border-primary/20 hover:border-primary/50 overflow-hidden py-0 transition-colors">
-                <div className="aspect-2/3 w-full overflow-hidden">
-                  <Image
-                    width={300}
-                    height={450}
-                    src={movie.poster || "./placeholder.svg"}
-                    alt={movie.title}
-                    className="h-full w-full object-cover transition-transform duration-300 hover:scale-105"
-                  />
-                </div>
-              </Card>
-            </Link>
-          </div>
-        ))}
-      </div>
+      ))}
     </div>
   );
 }
